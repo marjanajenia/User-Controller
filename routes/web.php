dd571549624d48994a45116f7c2c3309ef27f0b1
marjanajenia <marjanajenia98@gmail.com>
2022-12-05 23:23:30 +0600

    Dynamic URL Update and Structure Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\WebsiteController;
+use App\Http\Controllers\Backend\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+//<----------------webSite---------------->
+Route::get('/', [WebsiteController::class, 'webSiteView'])->name('web.home');
+
+//<----------------Admin---------------->
+Route::prefix('admin')->group(function(){
+    Route::controller(AdminController::class)->group(function(){
+        Route::get('/dashboard','dashboard')->name('admin.dashboard');
+        Route::get('/blank','blank')->name('admin.blank');
+        Route::get('/formdesign', 'formDesign')->name('admin.formdesign');
+    });
+});
 
 Route::get('/dashboard', function () {
     return view('backend.dashboard');
